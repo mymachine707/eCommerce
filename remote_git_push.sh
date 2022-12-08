cead8560e24ac50d0af8adc66172b0f27726c6d6
mymachine707 <mymaxs441@gmail.com>
2022-12-08 11:25:02 +0500

    order migration add but have mistake

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,21 +1,21 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"new module add"
+git commit -m"order migration add but have mistake"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"new module add"
+git commit -m"order migration add but have mistake"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"new module add"
+git commit -m"order migration add but have mistake"
 git push
 cd ..
 
 git add .
-git commit -m"new module add"
+git commit -m"order migration add but have mistake"
 git push
