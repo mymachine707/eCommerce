47c2dbc6927fcfdf05c591970d5e1a35e40b263c
mymachine707 <mymaxs441@gmail.com>
2022-12-08 16:08:46 +0500

    git commit successfuly

--- w_protogen.sh
+++ w_protogen.sh
@@ -15,5 +15,5 @@ cp -r protogen ./category_service/
 rm -rf ./order_service/protogen
 cp -r protogen ./order_service/
 
-rm -rf ./authorization_client
-cp -r protogen ./authorization_client/
+# rm -rf ./authorization_client/protogen
+# cp -r protogen ./authorization_client/
