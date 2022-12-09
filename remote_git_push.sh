e883b6778f447603192e2c1d51844780b55eab54
mymachine707 <mymaxs441@gmail.com>
2022-12-09 20:44:41 +0500

    getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"orderga full name qo'shildi va ishlavotti"
+git commit -m"getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"orderga full name qo'shildi va ishlavotti"
+git commit -m"getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"orderga full name qo'shildi va ishlavotti"
+git commit -m"getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"orderga full name qo'shildi va ishlavotti"
+git commit -m"getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti"
 git push
 cd ..
 
 git add .
-git commit -m"orderga full name qo'shildi va ishlavotti"
+git commit -m"getOrderById ga product ma'lumotlarini oladigan qilib o'zgartirildi. Order service category servicega klent bo'lib category servicedan product ma'lumotlarini o'ziga tortib olvotti"
 git push
