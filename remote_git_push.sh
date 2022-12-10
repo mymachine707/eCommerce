62406f8d443569bc5449584de56be95c85486e6c
mymachine707 <mymaxs441@gmail.com>
2022-12-10 09:47:38 +0500

    servicela hammasi tekshirildi orderlayam ishlavotti

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"servicela hammasi tekshirildi faqat order update bilan order delete qoldi to'g'irlanadigan."
+git commit -m"servicela hammasi tekshirildi orderlayam ishlavotti"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"servicela hammasi tekshirildi faqat order update bilan order delete qoldi to'g'irlanadigan."
+git commit -m"servicela hammasi tekshirildi orderlayam ishlavotti"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"servicela hammasi tekshirildi faqat order update bilan order delete qoldi to'g'irlanadigan."
+git commit -m"servicela hammasi tekshirildi orderlayam ishlavotti"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"servicela hammasi tekshirildi faqat order update bilan order delete qoldi to'g'irlanadigan."
+git commit -m"servicela hammasi tekshirildi orderlayam ishlavotti"
 git push
 cd ..
 
 git add .
-git commit -m"servicela hammasi tekshirildi faqat order update bilan order delete qoldi to'g'irlanadigan."
+git commit -m"servicela hammasi tekshirildi orderlayam ishlavotti"
 git push
