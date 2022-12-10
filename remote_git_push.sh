87b735a7285715b2f4d1860dfb15c14ceb9c537d
mymachine707 <mymaxs441@gmail.com>
2022-12-10 17:26:53 +0500

    client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"yangidan yozib chiqildi"
+git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"yangidan yozib chiqildi"
+git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"yangidan yozib chiqildi"
+git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"yangidan yozib chiqildi"
+git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
 git push
 cd ..
 
 git add .
-git commit -m"yangidan yozib chiqildi"
+git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
 git push
