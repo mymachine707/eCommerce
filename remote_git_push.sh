7e9429c144581f08d82cfd4eff318d9231cdd274
mymachine707 <mymaxs441@gmail.com>
2022-12-11 10:44:16 +0500

    authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
+git commit -m"authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
+git commit -m"authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
+git commit -m"authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
+git commit -m"authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi"
 git push
 cd ..
 
 git add .
-git commit -m"client service authorization uchun tayyorlandi. hash va JWT generatsiya qilishlari servicega yozildi"
+git commit -m"authorization qo'shilidi va ishlashi tekshirildi. Qo'shimcha qo'shadigan joylari bor. Product Categoryaga faqat sudo ga permission berdim. Client uchun client_service getclient list delete client list qilishga ruhsat berilmadi"
 git push
