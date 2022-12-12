ff9753dcff9f2e374b7ffaa39b274cee0bf1bb90
mymachine707 <mymaxs441@gmail.com>
2022-12-12 22:47:45 +0500

    Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim"
 git push
 cd ..
 
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Servicedan turib product service bilan authClient servicega nisbatan client ochdim va muvaffaqiyatli ulandim"
 git push
