f53dfb7d55c84554bc872bef75ff124384eeb28f
mymachine707 <mymaxs441@gmail.com>
2022-12-12 09:16:47 +0500

    Clean Up 2

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Clean Up 1"
+git commit -m"Clean Up 2"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Clean Up 1"
+git commit -m"Clean Up 2"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Clean Up 1"
+git commit -m"Clean Up 2"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Clean Up 1"
+git commit -m"Clean Up 2"
 git push
 cd ..
 
 git add .
-git commit -m"Clean Up 1"
+git commit -m"Clean Up 2"
 git push
