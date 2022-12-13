a0eba3517f3534e0d6363fa9b52fc1019b670f17
mymachine707 <mymaxs441@gmail.com>
2022-12-13 10:56:49 +0500

    Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi"
 git push
 cd ..
 
 git add .
-git commit -m"Clean Up 2"
+git commit -m"Order Item List ko'rgan paytda product va cliet ma'lumotlari birga keladigan qilindi"
 git push
