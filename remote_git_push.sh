fd1ae33edf92cad64db8eb418b4ae4b20bee190b
mymachine707 <mymaxs441@gmail.com>
2022-12-13 17:49:37 +0500

    Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"product bilan clientga orderdan turib client bo'ldim, clean up qisman qilindi"
+git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"product bilan clientga orderdan turib client bo'ldim, clean up qisman qilindi"
+git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"product bilan clientga orderdan turib client bo'ldim, clean up qisman qilindi"
+git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"product bilan clientga orderdan turib client bo'ldim, clean up qisman qilindi"
+git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
 git push
 cd ..
 
 git add .
-git commit -m"product bilan clientga orderdan turib client bo'ldim, clean up qisman qilindi"
+git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
 git push
