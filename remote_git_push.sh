afdbc44b0d52799dc3eebb858ff35f84d23a3821
mymachine707 <mymaxs441@gmail.com>
2022-12-14 16:42:26 +0500

    Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Yangi order service Api GETWayga ulandi"
+git commit -m"Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Yangi order service Api GETWayga ulandi"
+git commit -m"Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Yangi order service Api GETWayga ulandi"
+git commit -m"Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Yangi order service Api GETWayga ulandi"
+git commit -m"Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor"
 git push
 cd ..
 
 git add .
-git commit -m"Yangi order service Api GETWayga ulandi"
+git commit -m"Order service swaggerga ulandi. Authorization yoqildi hammasi ishlavotti. Clean up ga tayyor"
 git push
