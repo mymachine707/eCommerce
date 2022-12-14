55831be3e3756292d159fdfb5bcdc1d62a17fa2a
mymachine707 <mymaxs441@gmail.com>
2022-12-14 12:39:51 +0500

    Order service yangidan yozildi. hali getwayga ulanmagan

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
+git commit -m"Order service yangidan yozildi. hali getwayga ulanmagan"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
+git commit -m"Order service yangidan yozildi. hali getwayga ulanmagan"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
+git commit -m"Order service yangidan yozildi. hali getwayga ulanmagan"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
+git commit -m"Order service yangidan yozildi. hali getwayga ulanmagan"
 git push
 cd ..
 
 git add .
-git commit -m"Yengilanishla qo'shildi hamma method tekshirilvotti ishlavotti. Faqat order create qiganda order response javob qaytarmiyapti. Lekin bazaga yozvotti. Qogan hammasi joyida"
+git commit -m"Order service yangidan yozildi. hali getwayga ulanmagan"
 git push
