6cde901ad7cfc72b9fd3ab4567e924da5d3454ce
mymachine707 <mymaxs441@gmail.com>
2022-12-15 21:08:47 +0500

    Clean up news

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"order updated"
+git commit -m"Clean up news"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"order updated"
+git commit -m"Clean up news"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"order updated"
+git commit -m"Clean up news"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"order updated"
+git commit -m"Clean up news"
 git push
 cd ..
 
 git add .
-git commit -m"order updated"
+git commit -m"Clean up news"
 git push
