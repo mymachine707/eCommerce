8b258986925f80340cc56c6f5fae813b0964a065
mymachine707 <mymaxs441@gmail.com>
2022-12-15 12:19:59 +0500

    Orderga status va created at qo'shildi

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"Clean up 1"
+git commit -m"Orderga status va created at qo'shildi"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"Clean up 1"
+git commit -m"Orderga status va created at qo'shildi"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"Clean up 1"
+git commit -m"Orderga status va created at qo'shildi"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"Clean up 1"
+git commit -m"Orderga status va created at qo'shildi"
 git push
 cd ..
 
 git add .
-git commit -m"Clean up 1"
+git commit -m"Orderga status va created at qo'shildi"
 git push
