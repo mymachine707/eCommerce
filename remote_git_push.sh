f45978d0f47e223d4ba6cd123233d2ad864908bf
mymachine707 <mymaxs441@gmail.com>
2022-12-15 12:42:20 +0500

    order updated

--- remote_git_push.sh
+++ remote_git_push.sh
@@ -1,27 +1,27 @@
 cd eCommerce_apiGETWAY/
 git add .
-git commit -m"1"
+git commit -m"order updated"
 git push
 cd ..
 
 cd category_service/
 git add .
-git commit -m"1"
+git commit -m"order updated"
 git push
 cd ..
 
 cd order_service/
 git add .
-git commit -m"1"
+git commit -m"order updated"
 git push
 cd ..
 
 cd authorizationClient/
 git add .
-git commit -m"1"
+git commit -m"order updated"
 git push
 cd ..
 
 git add .
-git commit -m"1"
+git commit -m"order updated"
 git push
